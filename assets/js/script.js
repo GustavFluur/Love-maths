2e2246c02a22dc901709b549d8caa265aed5279a
GustavFluur <gustavfluur@hotmail.com>
2022-03-14 07:44:53 +0000

    Added Maths into function runGame element Math.floor(Math.random() *25) + 1;

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,25 @@
-function runGame() {
 
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        });
+    }
+});
+
+function runGame() {
+// Creates two random numbers between 0 and 25
+let num1 = Math.floor(Math.random() *25) + 1;
+let num2 = Math.floor(Math.random() *25) + 1;
 }
 
 function checkAnswer() {
@@ -28,3 +48,6 @@ function displaySubtractQuestion() {
 function displayMultiplyQuestion() {
     
 }
+
+// It's important to note that we said before, 
+// that we're trying to avoid creating too many variables in the global scope. 
